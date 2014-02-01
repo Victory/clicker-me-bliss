8b8940a9a84cee233c90ae6afd9e5304f2fcb798
vic <vic@>
2014-02-01 14:05:34 -0500

    create a clicker to convert resource to good

---
+++ src/js/items.js
@@ -0,0 +1,27 @@
+var Gitems = {
+  i1: {
+    name: '1 more clicks',
+    type: 'resource',
+    price: {
+      r1: 5
+    },
+    resource: 'r1',
+    modifier: 1,
+    priceJump: 1.1,
+    owned: 0
+  },
+  g1: {
+    name: "convert resource 1 to good 1",
+    type: 'good',
+    price: {
+      r1: 5
+    },
+    resource: 'r1',
+    resourceCost: 5,
+    winRate: 0.5,
+    good: 'g1',
+    modifier: 'r1',
+    priceJump: 1.10,
+    owned: 0
+  }
+};
