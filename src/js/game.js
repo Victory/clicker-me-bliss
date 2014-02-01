4fced5acea30677c3ab8ab63ea6e719e0ab62163
vic <vic@>
2014-02-01 06:08:10 -0500

    refactor click

--- src/js/game.js
+++ src/js/game.js
@@ -1,14 +1,21 @@
 var Game = function() {
   this.r1 = 0;
+  this.totalClicks = 0;
+
+  this.click = function(id) {
+    return function (evt) {
+      this.totalClicks++;
+      this[id]++;
+      num('t' + id, this[id]);
+    }.bind(this);
+  };
+
+  this.bindClick= function(id) {
+    bind(gId(id),'click', this.click(id));
+  };
 
   var constructor = (function (g) {
-    bind(
-      gId('r1'),
-      'click',
-      function() {
-        g.r1++;
-        num('tr1', g.r1);
-      });
+    g.bindClick('r1');
   }(this));
 };
 
