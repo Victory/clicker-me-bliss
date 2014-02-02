001d88c2de55e4c460823374b12de13cbfc8ff13
Victory <git@dfhu.org>
2014-02-02 06:40:21 -0500

    add an item that costs 2 different kinds of resources

--- src/js/items.js
+++ src/js/items.js
@@ -33,6 +33,18 @@ var Gitems = {
     priceJump: 1.1,
     owned: 0
   },
+  i3: {
+    name: '1 more clicks on r2',
+    type: 'item',
+    price: {
+      r1: 5,
+      r2: 5
+    },
+    resource: 'r1',
+    modifier: 2,
+    priceJump: 1.1,
+    owned: 0
+  },
   g1: {
     name: "convert resource 1 to good 1",
     type: 'good',
