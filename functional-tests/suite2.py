34efb0f5b9919c25754aa51231d6a91ac5a11426
vic <vic@>
2014-02-05 20:56:28 -0500

    parse resources and items, set class elms, make sure that all the elms are WebElement

--- functional-tests/suite2.py
+++ functional-tests/suite2.py
@@ -17,13 +17,9 @@ class Suite2(object):
     elms = {}
 
     def __init__(self):
-
-        self.read_items()
-        return
-
         self.driver = webdriver.Firefox()
         self.driver.get(HOME + "index.html")
-        self.elms['psr1'] = self.driver.find_element_by_id('psr1')
+        self.read_items()
 
     def read_items(self):
         f = open(BASEDIR + "/src/js/items.js", 'r')
@@ -32,13 +28,23 @@ class Suite2(object):
         items = json.loads(items)
 
         obj = items
-        for ka, va in obj.iteritems():
-            print ka, va
-            for kb, vb in va.iteritems():
-                if type(vb) == dict:
-                    for kc, vc in vb.iteritems():
-                        print kc, vc
-                print '  ' + kb, vb
+        for kk, vv in obj.iteritems():
+            self.elms[kk] = self.driver.find_element_by_id(kk)
+            if vv['type'] == "resouces":
+                for prefix in ['t', 's', 'ps', 'ms']:
+                    self.elms[prefix + kk] = \
+                        self.driver.find_element_by_id(prefix + kk)
+
+            if vv['type'] == "item":
+                self.elms['o' + kk] = self.driver.find_element_by_id('o' + kk)
+                for resource in vv['price']:
+                    self.elms['p' + kk + resource] = \
+                        self.driver.find_element_by_id('p' + kk + resource)
+
+        # check that all elms are in fact WebElement
+        for elm in self.elms:
+            assert type(self.elms[elm]) == \
+                webdriver.remote.webelement.WebElement
 
     def __getattr__(self, name):
         if name in self.elms:
