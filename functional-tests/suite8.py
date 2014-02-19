7e327bdb60c251eb8e01d5b4390380a9bc596ca7
vic <vic@>
2014-02-18 19:59:39 -0500

    test the assertsEquals test

---
+++ functional-tests/suite8.py
@@ -0,0 +1,18 @@
+# suite to test types of assertions
+from clickerft.cft import Cft
+
+class Suite(Cft):
+    def __init__(self):
+        for name in dir(self):
+            if name[0:5] == 'test_':
+                getattr(self, name)()
+
+    def test_asserts_equals(self):
+        self.assertEquals(1, 1, "should equal")
+
+    def test_fail_assert_equals(self):
+        result = self.assertEquals(1, 2, "should fail")
+        assert not result
+
+if __name__ == '__main__':
+    Suite()
