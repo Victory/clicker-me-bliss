2e1f2c714a8483f8c11f4298e441bae323334eec
vic <vic@>
2014-02-20 19:36:34 -0500

    accept but not yet run a cheatCodes function with suite9 test

---
+++ functional-tests/suite9.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+
+from clickerft.cft import Cft
+
+
+class Suite(Cft):
+    def setUpTest(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get(self.home + "index.html")
+        self.read_items()
+
+    def test_cheat_function_is_written_to_div(self):
+        cc = self.driver.find_element_by_id("cheatCodes")
+        cf = self.driver.find_element_by_id("runningCheat")
+
+        function_string = "function() { console.log('yay!') }"
+        cc.send_keys(function_string)
+        self.driver.find_element_by_id('pausePlay').click()
+
+        assert cf.text == function_string
+
+if __name__ == '__main__':
+    Suite()
