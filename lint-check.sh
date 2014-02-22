bbecedfc7082174800102bdf31c0ac34084227ce
Victory <git@dfhu.org>
2014-02-22 03:12:14 -0500

    make lint-check more explicit and make it check only files that show up in `git status`

--- lint-check.sh
+++ lint-check.sh
@@ -3,11 +3,23 @@
 LINT="pocketlint"
 cd "$(git rev-parse --show-toplevel)"
 CWD=$(pwd)
-JSPATH="./src/js/*js"
-HTMLPATH="./src/*.html"
-CSSPATH="./src/css/*css"
-PYTHONPATH="./functional-tests/*py"
 
-$LINT  $JSPATH
-$LINT  $HTMLPATH
-$LINT $PYTHONPATH
+JSPATH=$(git status --porcelain | grep ".*js" | awk '{print $2}')
+HTMLPATH=$(git status --porcelain | grep ".*html" | awk '{print $2}')
+CSSPATH=$(git status --porcelain | grep ".*css" | awk '{print $2}')
+PYTHONPATH=$(git status --porcelain | grep ".*py" | awk '{print $2}')
+
+
+if [ ! -z $JSPATH ]; then
+    echo $JSPATH
+    $LINT  $JSPATH
+fi
+if [ ! -z $HTMLPATH ]; then
+    echo $HTMLPATH
+    $LINT  $HTMLPATH
+fi
+
+if [ ! -z $PYTHONPATH ]; then
+    echo $PYTHONPATH
+    $LINT $PYTHONPATH
+fi
