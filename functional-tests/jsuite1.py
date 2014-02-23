c7da5a1f18648ff002ed4f648bf39854905835ff
Victory <git@dfhu.org>
2014-02-23 10:03:37 -0500

    test running javascript using `document` to store state

--- functional-tests/jsuite1.py
+++ functional-tests/jsuite1.py
@@ -1,4 +1,5 @@
 from clickerft.jsplayer import JsPlayer
+from time import sleep
 
 
 class JSuite(JsPlayer):
@@ -9,6 +10,28 @@ document.getElementById("r1").click()
 """
         self.run_javascript(js)
         assert int(self.tr1.text) > 0
+
+    def test_js_with_state(self):
+        js = """
+cheats = document.cheats || {};
+document.cheats = cheats;
+if (! cheats.runR1 ) {
+  cheats.runR1 = 0;
+}
+
+if (cheats.runR1 < 5) {
+  document.getElementById("r1").click();
+  cheats.runR1 +=  1;
+  return;
+} else {
+  console.log('skipping r1');
+}
+"""
+        self.run_javascript(js)
+
+        # allow time for the cheat codes to run
+        sleep(3)
+        assert int(self.tr1.text) == 5
 
 if __name__ == '__main__':
     JSuite()
