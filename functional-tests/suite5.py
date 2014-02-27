6cbc4c50a590d4fc69af971d0331101ba0a3b970
Victory <git@dfhu.org>
2014-02-27 18:16:57 -0500

    make suit5 pass

--- functional-tests/suite5.py
+++ functional-tests/suite5.py
@@ -1,11 +1,17 @@
+from time import sleep
+
 from clickerft.cft import Cft
 
 
 class Suite5(Cft):
+
     def test_buy_clicks_per_generation(self):
         price = int(self.pincreaseMaxClicks.text)
         start = int(self.clicksPerGeneration.text)
+        clickwait = .5
         for i in xrange(price):
+            while int(self.clicksOwned.text) < 1:
+                sleep(clickwait)
             self.increaseClicksPerGeneration.click()
         fin = int(self.clicksPerGeneration.text)
         assert 1 == fin - start
