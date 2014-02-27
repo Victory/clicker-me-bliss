d6ef24ad59fa1ae708c816061af28c1a0a910a4c
vic <vic@>
2014-02-27 18:09:28 -0500

    make suite4 pass

--- functional-tests/suite4.py
+++ functional-tests/suite4.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from clickerft.cft import Cft
 
 
@@ -6,7 +8,10 @@ class Suite4(Cft):
     def test_buy_max_clicks(self):
         price = int(self.pincreaseMaxClicks.text)
         start = int(self.maxClicks.text)
+        clickwait = .5
         for i in xrange(price):
+            while int(self.clicksOwned.text) < 1:
+                sleep(clickwait)
             self.increaseMaxClicks.click()
         fin = int(self.maxClicks.text)
         assert 1 == fin - start
