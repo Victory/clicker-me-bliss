9e4d17d6394fc182a3ea47e58d705f6e2a77f949
Victory <git@dfhu.org>
2014-02-28 18:51:57 -0500

    silence suite8 without breaking tests

--- functional-tests/suite8.py
+++ functional-tests/suite8.py
@@ -1,4 +1,7 @@
 # suite to test types of assertions
+import sys
+import cStringIO
+
 from clickerft.cft import Cft
 
 
@@ -12,7 +15,11 @@ class Suite(Cft):
         self.assertEquals(1, 1, "should equal")
 
     def test_fail_assert_equals(self):
+        save_stdout = sys.stdout
+        sys.stdout = cStringIO.StringIO()
         result = self.assertEquals(1, 2, "should fail")
+        sys.stdout = save_stdout
+
         assert not result
 
 if __name__ == '__main__':
